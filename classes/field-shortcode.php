17f380da672d2257eda622785ac84c962f0eb8f4
Thomas Barregren <thomas@kntnt.se>
2020-09-23 11:54:13 +0200

    Add comments.

--- classes/field-shortcode.php
+++ classes/field-shortcode.php
@@ -7,12 +7,19 @@ class Field_Shortcode {
 
   private static $type_count = [];
 
+  // Map of allowed attributes. The first key is an attribute. If the value of
+  // the attribute isn't an array, it an be used with all elemets with the
+  // provided default value. If the value of the attribute is an array, the
+  // keys of that array correspond to input types for which the attribute is
+  // allowed, and the values of that array is corresponding default values.
   private static $defaults = [
     'checked' => [ 'radio' => false, 'checkbox' => false ],
     'class' => null,
     'cols' => [ 'textarea' => 20 ],
+    'description' => null,
     'disabled' => null,
     'id' => null,
+    'label' => null,
     'max' => [ 'date' => null, 'month' => null, 'week' => null, 'time' => null, 'datetime-local' => null, 'number' => null, 'range' => null ],
     'maxlength' => [ 'password' => null, 'search' => null, 'tel' => null, 'text' => null, 'url' => null ],
     'min' => [ 'date' => null, 'month' => null, 'week' => null, 'time' => null, 'datetime-local' => null, 'number' => null, 'range' => null ],
@@ -34,18 +41,22 @@ class Field_Shortcode {
     'step' => [ 'date' => null, 'month' => null, 'week' => null, 'time' => null, 'datetime-local' => null, 'number' => null, 'range' => null ],
     'style' => null,
     'type' => null,
-    // 'value' => null,
   ];
 
+  // Array of supported fields and their templates.
+  // Following placeholders in the templates will be replaced:
+  //   - {id}         => the fields id attribute
+  //   - {value}      => the value/content of the field
+  //   - {attributes} => all attributes except id and value
   private static $templates = [
-    'textarea' => '<textarea id="{id}" name="{id}" {attributes}>{content}</textarea>',
-    'text' => '<input type="text" id="{id}" name="{id}" value="{content}" {attributes}>',
-    'email' => '<input type="email" id="{id}" name="{id}" value="{content}" {attributes}>',
-    'url' => '<input type="url" id="{id}" name="{id}" value="{content}" {attributes}>',
-    'tel' => '<input type="tel" id="{id}" name="{id}" value="{content}" {attributes}>',
-    'hidden' => '<input type="hidden" id="{id}" name="{id}" value="{content}" {attributes}>',
-    'html' => '<div id="{id}" {attributes}>{content}</div>',
-    'submit' => '<input type="submit" value="{content}" {attributes}>',
+    'textarea' => '<textarea id="{id}" name="{id}" {attributes}>{value}</textarea>',
+    'text' => '<input type="text" id="{id}" name="{id}" value="{value}" {attributes}>',
+    'email' => '<input type="email" id="{id}" name="{id}" value="{value}" {attributes}>',
+    'url' => '<input type="url" id="{id}" name="{id}" value="{value}" {attributes}>',
+    'tel' => '<input type="tel" id="{id}" name="{id}" value="{value}" {attributes}>',
+    'hidden' => '<input type="hidden" id="{id}" name="{id}" value="{value}" {attributes}>',
+    'html' => '<div id="{id}" {attributes}>{value}</div>',
+    'submit' => '<input type="submit" value="{value}" {attributes}>',
   ];
 
   public function run() {
@@ -58,28 +69,43 @@ class Field_Shortcode {
 
   public function shortcode( $atts, $content ) {
 
-    if ( ! empty( $atts['type'] ) ) {
+    // Leave shortcode as it is if its type is not provided.
+    if ( empty( $atts['type'] ) ) {
+      return $content;
+    }
 
-      $atts = Plugin::shortcode_atts( self::defaults( $atts['type'] ), $atts );
-      $atts = array_filter( $atts, function( $att ) { return null !== $att; } );
-      $atts = Plugin::esc_attrs( $atts );
-
-      if ( empty( $atts['id'] ) ) {
-        if ( empty( self::$type_count[$atts['type']] ) ) {
-          self::$type_count[$atts['type']] = 0;
-        }
-        $atts['id'] = $atts['type'] . '-' . ++self::$type_count[$atts['type']];
+    // Add `id` if missing. 
+    if ( empty( $atts['id'] ) ) {
+      if ( empty( self::$type_count[$atts['type']] ) ) {
+        self::$type_count[$atts['type']] = 0;
       }
+      $atts['id'] = $atts['type'] . '-' . ++self::$type_count[$atts['type']];
+    }
 
-      $content = do_shortcode( $content );
-      $content =  self::field( $atts, $content );
+    // Remove unsupported attributes and add defalt values for missing ones.
+    $atts = Plugin::shortcode_atts( self::defaults( $atts['type'] ), $atts );
 
-    }    
+    // Fetch label and description and remove them from the list of attributes.
+    $label = Plugin::peel_off( 'label', $atts );
+    $description = Plugin::peel_off( 'description', $atts );
+
+    // Remaning attributes will be used as HTML attributes. Remove those with
+    // no value, and trim and escape the other.
+    $atts = array_filter( $atts, function( $att ) { return null !== $att; } );
+    $atts = Plugin::esc_attrs( $atts );
+
+    // Execute any shortcodes in the enclosed content.
+    $content = do_shortcode( $content );
+
+    // Create a HTML from field.
+    $content =  self::field( $atts, $content, $label, $description );
 
     return $content;
 
   }
 
+  // Returns an array of attributes and their default values for
+  // a field of the provied type.
   private static function defaults( $type ) {
     $defaults = [];
     foreach ( self::$defaults as $att => $val ) {
@@ -95,6 +121,7 @@ class Field_Shortcode {
     return $defaults;
   }
 
+  // Returns a HTML form field.
   private static function field( $atts, $content ) {
     $out = '';
     $type = Plugin::peel_off( 'type', $atts );
@@ -102,7 +129,7 @@ class Field_Shortcode {
       $out = strtr( self::$templates[$type], [
         '{id}' => Plugin::peel_off( 'id', $atts ),
         '{attributes}' => Plugin::attributes( $atts ),
-        '{content}' => $content,
+        '{value}' => $content,
       ] );
     }
     return $out;
--- classes/form-shortcode.php
+++ classes/form-shortcode.php
@@ -5,15 +5,22 @@ namespace Kntnt\Form_Shortcode;
 
 class Form_Shortcode {
 
+  // Array of supported attributes and heirdefault values.
   private static $defaults = [
     'id' => null,
     'class' => null,
     'style' => null,
     'no_br' => true,
+    'post' => null,
+    'success' => null,
   ];
 
+  // Template for generating the form elemet.
+  // Following placeholders in the template will be replaced:
+  //   - {attributes} => the attributes of the form
+  //   - {content}    => the content of the form
   private static $template = "<form method=\"post\" {attributes}>{content}</form>";
-        
+
   public function run() {
 
     add_filter( 'no_texturize_shortcodes', function ( $shortcodes ) {
@@ -27,19 +34,34 @@ class Form_Shortcode {
 
   public function shortcode( $atts, $content ) {
 
+    // Remove unsupported attributes and add defalt values for missing ones.
     $atts = Plugin::shortcode_atts( self::$defaults, $atts );
+
+    // Fetch post, show and no_br remove them from the list of attributes.
+    $post = Plugin::peel_off( 'post', $atts );
+    $show = Plugin::peel_off( 'show', $atts );
+    $no_br = Plugin::peel_off( 'no_br', $atts );
+
+    // Remaning attributes will be used as HTML attributes. Remove those with
+    // no value, and trim and escape the other.
     $atts = array_filter( $atts, function( $att ) { return null !== $att; } );
     $atts = Plugin::esc_attrs( $atts );
 
-    $no_br = Plugin::peel_off( 'no_br', $atts );
+    // Execute field shortcodes in the enloced content.
+    Plugin::instance( 'Field_Shortcode' )->run(); // Add field shortcode
+    $content = do_shortcode( $content ); // Process shortcodes in content.
+    Plugin::instance( 'Field_Shortcode' )->halt(); // Remove field shortcode
 
-    
-    Plugin::instance( 'Field_Shortcode' )->run();
-    $content = do_shortcode( $content );
-    Plugin::instance( 'Field_Shortcode' )->halt();
-
+    // Create a HTML form.
     $content = self::form( $atts, $content );
 
+    // Typically, a user will put field shortcodes on separate lines.
+    // Wordpress' infamous wpautop() will interpret each line as empty, thus
+    // adding <br /> into HTML. The effect is a rudimentary styling of the
+    // form, which is a good thing for uses who don't can/want to add their
+    // own CSS. But it is unnecessary and usually undesirable when one is
+    // styling the form. Hence the option to remove them with the shortcode
+    // attribute `no_br`.
     if ( $no_br ) {
       $content = strtr( $content, [ '<br />' => '' ] );
     }
@@ -48,6 +70,7 @@ class Form_Shortcode {
 
   }
 
+  // Generate the HTML.
   private static function form( $atts, $content ) {
     $out = '';
     if ( $content ) {
